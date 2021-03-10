4bd66c4ce85231d7111f415f9f236ac644050960
Seth <sethvance26@gmail.com>
2021-03-10 13:13:34 -0500

    Changing all files to contain starter code

---
+++ Develop/script.js
@@ -0,0 +1,45 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var specialCharacters = 
+var lowerCase = 
+var upperCase =
+// need to make multiple arrays outside the function for different
+// type of characters
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+function generatePassword() {
+  return "Hello" 
+  // need to generate a random string,
+  // 
+
+
+  // 
+}
+
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// Most can go into generatePassword
+
+
+
+
+
+
+
+
+
+
+
+
+// 
