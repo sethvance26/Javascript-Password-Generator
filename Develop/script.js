b846dd96b5ee4f3676d931788366340f9b466572
Seth <sethvance26@gmail.com>
2021-03-12 20:57:11 -0500

    changing js file

--- Develop/script.js
+++ Develop/script.js
@@ -1,56 +1,76 @@
-// Line three is making a variable (generateBtn) for the ID of generate
- 
+var specialCharacter = ['@', '%', '+','/',"'",'!','#','$','^','?',';',',',')','(','}','{','}','{','~','-','_','.',];
+
+var lower_chars = ["abcdefghijklmnopqrstuvwxyz"];
+
+var upper_chars = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+
+var numbers = ["1234567890"];
+
 var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", generatePassword);
 
-var collectionOfLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()~+=-_<>?[{]},";
-// var specialChar = "!@#$%^&*()?:;-+-";
+var userChoice = [];  //user input for confirms
 
-// var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var password = ""; //This is the password that gets displayed in textbox
 
-// var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// function writePassword() {
+//   var userChoice = window.prompt("Please enter how many characters you would like your password to consist of:");
+//   var password = generatePassword(userChoice);
+//   var passwordText = document.querySelector("#password");
+//   passwordText.value = password;
+// }
 
-// var numerals = "123456789";
-
-// // Above we made four new variables with arrays for special characters, 
-// lowercase and uppercase characters as well as numbers.
-
-function random() {
-
-
-
-    var randomChar = collectionOfLetters[Math.floor(Math.random() * collectionOfLetters.length)]
-
-    return randomChar;
-}
-
-
-function generatePassword(length) {
-  var password = ""; 
-  for (var i = 0; i < length; i++) {
-  var temp = random()
-  password += temp; 
-  console.log("loop", i, password );
-// need to generate a random string,
-}
-
-return password;
-}
-
-var minimumNumberOfCharacters= 8;
-var userPasswordlength = 32;
-
-
-
-// Write password to the #password input
-function writePassword() {
-  var userChoice = window.prompt("Please enter how many characters you would like your password to consist of:")
-  var password = generatePassword(userChoice);
+function generatePassword() {
+  var passLength = parseInt(window.prompt("How many characters would you like the password to be?"));
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
+  console.log(passLength);
+  if (!passLength) {
+    
+    return;
+}
+else if (passLength > 128) {
+  alert("it needs to be less than 129");
+  return;
+
+}
+else if (passLength < 8 ) {
+  alert("it needs to be more than 8");
+  return;
 }
 
+else {
+  var isSpecChosen = window.confirm("click OK to confirm including special characters");
+  console.log(isSpecChosen);
+  var isLowerCase = window.confirm("click OK to confirm including lowercase characters");
+  console.log(isLowerCase);
+  var isUpperCase = window.confirm("click OK to confirm including uppercase characters");
+  console.log(isUpperCase);
+  var isNumber = window.confirm("click OK to confirm including numeric characters");
+}
+if (!isSpecChosen && !isLowerCase && !isUpperCase && !isNumber) {
+  alert("You didn't choose a character type!");
+  return;
 
-// Add event listener to generate button
-var generateBtn = document.querySelector("#generate");
-generateBtn.addEventListener("click", writePassword);
+}
+if (!isSpecChosen){
+  userChoice = user.Choice.concat(specialCharacter);
+}
+if (isLowerCase){
+  userChoice = userChoice.concat(lower_chars);
+}
+if (isUpperCase){
+  userChoice = userChoice.concat(upper_chars);
+}
+if (isNumber){
+  userChoice = userChoice.concat(numbers);
+}
+console.log(userChoice);
+}
 
+function password(passLength) {
+  var password = password.length;
+  for ( var i = 0; i < passLength; i++) {
+  passLength += password.chatAt(Math.floor(Math.random() * userChoice));
+  }
+}
